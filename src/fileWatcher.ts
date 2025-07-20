2ea1164f036d99e5e298ea60d20fa736b00eeeb8
lujian1997524 <lujian1997524@users.noreply.github.com>
2025-07-20 10:04:46 +0800

    立即保存文件的当前修改时间（在添加注释之前）
    🐛 修复filewatcher相关问题
    
    🕒 2025-07-20 10:04:45

--- src/fileWatcher.ts
+++ src/fileWatcher.ts
@@ -1,13 +1,15 @@
 /**
- * 最后修改时间: 2025-07-20 09:35:51
- * 上次修改时间: 2025-07-20 09:35:31
- * 文件大小: 11103 bytes
+ * 最后修改时间: 2025-07-20 10:04:35
+ * 上次修改时间: 2025-07-20 10:04:31
+ * 文件大小: 11105 bytes
  */
 import * as vscode from 'vscode';
 import { TimestampProvider } from './timestampProvider';
 import { GitManager } from './gitManager';
 import { GitIgnoreManager } from './gitIgnoreManager';
 import * as path from 'path';
+
+
 
 export class FileWatcher implements vscode.Disposable {
     private watchers: vscode.FileSystemWatcher[] = [];
